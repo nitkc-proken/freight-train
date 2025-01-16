2dfeb63fa46fbd8a850a4a940254e140856a08c1
Naotiki <naotiki@naotiki.me>
2025-01-17 01:58:26 +0900

    feat: add Command trait

--- client/src/commands/example.rs
+++ client/src/commands/example.rs
@@ -1,11 +1,13 @@
+use super::{Args, Command};
+
 #[derive(clap::Parser, Debug)]
 pub struct Example {
     #[arg(short, long)]
     name: String,
 }
 
-impl Example {
-    pub async fn run(&self) {
+impl Command for Example {
+    async fn run(&self, _args: &Args) {
         println!("Hello, world! {:?}", self.name);
     }
 }
