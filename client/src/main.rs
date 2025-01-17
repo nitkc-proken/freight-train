893003c1cd183ca800215fcff7580f8cc54be87f
KouRo <git@kouro.dev>
2025-01-17 21:10:44 +0900

    Fix problem fixing conflict

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,13 @@
 use clap::Parser;
 use commands::Args;
+use config::Config;
 
 mod commands;
+mod config;
+
 #[tokio::main]
 async fn main() {
+    let _config = Config::load();
     let args = Args::parse();
     args.run().await;
 }
