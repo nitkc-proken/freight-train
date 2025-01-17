9443c696705f04b41305cad9c3735cb25fd383af
KouRo <git@kouro.dev>
2025-01-17 19:19:09 +0900

    Login command before http POST

---
+++ client/src/commands/login.rs
@@ -0,0 +1,31 @@
+use super::{Args, Command};
+use proconio::input;
+use serde::Serialize;
+use serde_json;
+use std::io::{self, Write};
+
+#[derive(clap::Parser, Debug)]
+pub struct Login {}
+
+impl Command for Login {
+    async fn run(&self, _args: &Args) {
+        print!("username: ");
+        io::stdout().flush().unwrap();
+        input! {username: String};
+        let password = rpassword::prompt_password("password: ").unwrap();
+        login(username, password).await;
+    }
+}
+
+async fn login(user: String, password: String) -> bool {
+    let login_request = LoginRequestBody { user, password };
+    let login_request_json = serde_json::to_string(&login_request).unwrap();
+    println!("{}", login_request_json);
+    true
+}
+
+#[derive(Debug, Serialize)]
+struct LoginRequestBody {
+    user: String,
+    password: String,
+}
