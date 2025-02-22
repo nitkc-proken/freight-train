497f3b0f91cec193feb68dbb32798a98e0b348d6
Naotiki <naotiki@naotiki.me>
2025-02-23 00:02:18 +0900

    refactor: remove unused code

--- common/src/protocol.rs
+++ common/src/protocol.rs
@@ -1,20 +1,9 @@
-use log::{error, info};
 use serde_derive::{Deserialize, Serialize};
-use std::error::Error;
-use std::io::{Read, Write};
-use std::net::{IpAddr, SocketAddr};
+use std::net::IpAddr;
 use tokio::io::AsyncRead;
-use tokio::net::UdpSocket;
 use tokio_stream::StreamExt;
-use tokio_util::bytes::{BufMut, Bytes, BytesMut};
+use tokio_util::bytes::{Bytes, BytesMut};
 use tokio_util::codec::{Decoder, Encoder, FramedRead};
-use tun::{Reader, Writer};
-
-#[derive(Serialize, Deserialize)]
-struct Capsule {
-    #[serde(with = "serde_bytes")]
-    data: Vec<u8>,
-}
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub enum Frame {
@@ -44,7 +33,6 @@ pub enum ResponseBody {
 pub enum SessionState {
     Init,
     Authenticated,
-    RequestingIPAddress,
     Ready,
     Established,
     Closed,
@@ -107,52 +95,3 @@ pub enum Protocol {
 }
 
 pub const USING_PROTOCOL: Protocol = Protocol::Tcp;
-
-pub async fn tun_to_udp(tun: &mut Reader, udp: &UdpSocket, peer_addr: &Option<SocketAddr>) {
-    let mut buffer = [0u8; 1500];
-    loop {
-        if let None = peer_addr {
-            tokio::time::sleep(tokio::time::Duration::from_millis(10)).await;
-            continue;
-        }
-        match tun.read(&mut buffer) {
-            Ok(n) => {
-                info!("read {} bytes from TUN", n);
-                let capsule = Capsule {
-                    data: buffer[..n].to_vec(),
-                };
-                let serialized_data = bincode::serialize(&capsule).unwrap();
-                udp.send_to(&serialized_data, peer_addr.unwrap())
-                    .await
-                    .unwrap();
-            }
-            Err(e) => {
-                error!("TUN read error: {}", e);
-            }
-        }
-    }
-}
-
-pub async fn udp_to_tun(
-    tun: &mut Writer,
-    udp: &UdpSocket,
-    peer_addr: Option<&mut Option<SocketAddr>>,
-) {
-    let mut buffer = [0u8; 1500];
-    loop {
-        match udp.recv_from(&mut buffer).await {
-            Ok((n, addr)) => {
-                if let Some(&mut ref mut a) = peer_addr {
-                    *(a) = Some(addr)
-                }
-                let capsule: Capsule = bincode::deserialize(&buffer[..n]).unwrap();
-                info!("read {} bytes from UDP", n);
-                tun.write_all(capsule.data.as_slice()).unwrap();
-                tun.flush().unwrap();
-            }
-            Err(e) => {
-                error!("UDP read error: {}", e);
-            }
-        }
-    }
-}
